9421ec3be6baef0995f0659dafd1d86fda46a608
WEGI_102 <wegidaany@gmail.com>
2020-08-10 17:37:55 -0300

    initial commit

---
+++ current.js
@@ -0,0 +1,25 @@
+// let btnGetdate = document.querySelector("button");
+// let output = querySelector("h2");
+// btnShow.addEventListener('click',()=>
+// {
+//     let month = today.getmonth();
+//     let year = today.getfullyear();
+//     let date = today.getdate();
+//     let currentdate=   `${month}/${date}/${year}`
+//     output.innertext=currentdate;
+
+// });
+// let dd = today.getDate(); 
+// let mm = today.getMonth() +1;
+// let yyyy = today.getFullYear();
+// console.log(mm);
+
+// if(dd <20){
+//     dd='0'+ dd;
+// }
+// if(mm <20){
+//     mm='0' + mm;
+// }
+// today= dd +':'+ mm +':'+ yyyy;
+// console.log(today);
+// document.getElementById("time").innerHTML= "current date"+today;
